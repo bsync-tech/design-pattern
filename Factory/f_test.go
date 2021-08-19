7b15a52321591e0426f7f4d2cb794da9201f106b
wangx <wangx@wangxdeMacBook-Pro.local>
2021-08-19 20:14:10 +0800

    end factory

--- Factory/f_test.go
+++ Factory/f_test.go
@@ -16,22 +16,16 @@ var _ = Suite(&MySuite{})
 
 func (s *MySuite) TestProduceDuck(c *C) {
 	bf := new(BeijingDuckFactory)
-	bfd := bf.ProduceDuck("BeijingDuck")
+	bfd := bf.ProduceDuck()
 	c.Assert(bf == nil, Equals, false)
 	c.Logf("BeijingDuckFactory produce 'BeijingDuck', name is %s", bfd.GetName())
 	c.Assert(bfd.GetName() == "BeijingDuck", Equals, true)
-	sfd := bf.ProduceDuck("ShaoxingDuck")
-	c.Logf("BeijingDuckFactory produce 'ShaoxingDuck', name is %s", sfd.GetName())
-	c.Assert(sfd.GetName() == "FakeShaoxingDuck", Equals, true)
 
 	sf := new(ShaoxingDuckFactory)
-	sfd = sf.ProduceDuck("ShaoxingDuck")
+	sfd := sf.ProduceDuck()
 	c.Assert(sfd == nil, Equals, false)
 	c.Logf("ShaoxingDuckFactory produce 'ShaoxingDuck', name is %s", sfd.GetName())
 	c.Assert(sfd.GetName() == "ShaoxingDuck", Equals, true)
-	bfd = sf.ProduceDuck("BeijingDuck")
-	c.Logf("ShaoxingDuckFactory produce 'BeijingDuck', name is %s", bfd.GetName())
-	c.Assert(bfd.GetName() == "FakeBeijingDuck", Equals, true)
 
 	fmt.Println(c.GetTestLog())
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module gof
 
 go 1.16
 
-require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+require (
+	github.com/rogpeppe/godef v1.1.2 // indirect
+	github.com/uudashr/gopkgs/v2 v2.1.2 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
