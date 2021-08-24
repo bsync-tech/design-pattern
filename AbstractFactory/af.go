e8daeee5c348f186a56b33996dd9e11f780407ab
bsync-tech <76582032+bsync-tech@users.noreply.github.com>
2021-08-24 22:44:24 +0800

    Add files via upload

--- AbstractFactory/af.go
+++ AbstractFactory/af.go
@@ -1,30 +1,78 @@
 package af
 
-// In this pattern, Add Duck type need, you need
-// 1. Add lines code in FactoryProduceDuck
-// 2. Add new type Duck in this file
+// In this pattern, If need produce new Duck Product, you need
+// 1. Add new type Duck in this file
+// 2. Add new DuckFactory type
 
-type Cook interface {
-	CookWithToast() string
-	CookWithBeer() string
+type Color interface {
+	GetName() string
+}
+
+type YelloDuck struct {
+	name string
+}
+
+func (y YelloDuck) GetName() string {
+	return y.name
+}
+
+type BlueDuck struct {
+	name string
+}
+
+func (b BlueDuck) GetName() string {
+	return b.name
+}
+
+type Shape interface {
+	GetName() string
+}
+
+type BigDuck struct {
+	name string
+}
+
+func (b BigDuck) GetName() string {
+	return b.name
+}
+
+type SmallDuck struct {
+	name string
+}
+
+func (s SmallDuck) GetName() string {
+	return s.name
+}
+
+type DuckFactory interface {
+	ProduceColor() Color
+	ProduceShape() Shape
 }
 
 type BeijingDuckFactory struct{}
 
-func (b BeijingDuckFactory) CookWithToast() string {
-	return "Cook with toast"
+func (b BeijingDuckFactory) ProduceColor() Color {
+	return BlueDuck{
+		name: "BlueDuck",
+	}
 }
 
-func (b BeijingDuckFactory) CookWithBeer() string {
-	return "Cook with beer"
+func (b BeijingDuckFactory) ProduceShape() Shape {
+	return BigDuck{
+		name: "BigDuck",
+	}
 }
 
 type ShaoxingDuckFactory struct{}
 
-func (s ShaoxingDuckFactory) CookWithBeer() string {
-	return "Cook with beer"
+func (s ShaoxingDuckFactory) ProduceColor() Color {
+	return BlueDuck{
+		name: "YelloDuck",
+	}
 }
 
-func (s ShaoxingDuckFactory) CookWithToastr() string {
-	return "Cook method invalid"
+func (s ShaoxingDuckFactory) ProduceShape() Shape {
+	return SmallDuck{
+		name: "SmallDuck",
+	}
 }
